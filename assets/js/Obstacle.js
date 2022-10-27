d60808984ca7388c1327e372bdad2657c3f50ab6
Christian Wiehl Hejlesen <wiehlhejlesen@outlook.dk>
2022-10-28 00:18:46 +0200

    Test bullet collision

--- assets/js/Obstacle.js
+++ assets/js/Obstacle.js
@@ -1,74 +1,92 @@
 /** @format */
 
 export default class Obstacle {
-	obstacle = [];
-	scale = 1;
-	destroyed = false;
+   obstacle = [];
+   scale = 1;
+   destroyed = false;
+   ready = false;
+   totalBlocks = 0;
 
-	constructor(posX, posY, color, playerBC, enemyBC, blockSize = 3) {
-		this.x = posX;
-		this.y = posY;
-		this.color = color;
-		this.playerBC = playerBC;
-		this.enemyBC = enemyBC;
-		this.blockSize = blockSize;
+   constructor(posX, posY, color, playerBC, enemyBC, blockSize = 3) {
+      this.x = posX;
+      this.y = posY;
+      this.color = color;
+      this.playerBC = playerBC;
+      this.enemyBC = enemyBC;
+      this.blockSize = blockSize;
 
-		fetch('../assets/json/obstacle.json')
-			.then(result => result.json())
-			.then(json => {
-				this.fullObstacle = json;
-				this.obstacle = this.fullObstacle;
-				this.width = this.fullObstacle.map[0].length * this.blockSize;
-				this.height = this.fullObstacle.map.length * this.blockSize;
-			});
-	}
+      fetch('../assets/json/obstacle.json')
+         .then((result) => result.json())
+         .then((json) => {
+            this.fullObstacle = json;
+            this.obstacle = this.fullObstacle;
+            this.width = this.fullObstacle.map[0].length * this.blockSize;
+            this.height = this.fullObstacle.map.length * this.blockSize;
+            this.totalBlocks = this.countBlocks();
+            this.ready = true;
+         });
+   }
 
-	draw(ctx) {
-		if (this.destroyed) return;
-		this.obstacle.forEach((column, y) => {
-			column.forEach((row, x) => {
-				ctx.fillStyle = this.color;
-				if (row === '@') {
-					ctx.fillRect(x * this.blockSize + this.x, y * this.blockSize + this.y, this.blockSize, this.blockSize);
-				}
-			});
-		});
-	}
+   draw(ctx) {
+      if (!this.ready) return;
+      this.destroyed = this.totalBlocks !== 0 ? false : true;
+      if (this.destroyed) return;
+      for (let y = 0; y < this.obstacle.map.length; y++) {
+         for (let x = 0; x < this.obstacle.map[y].length; x++) {
+            ctx.fillStyle = this.color;
+            if (this.obstacle.map[y][x] !== '#') continue;
+            ctx.fillRect(
+               x * this.blockSize * this.scale + this.x,
+               y * this.blockSize * this.scale + this.y,
+               this.blockSize * this.scale,
+               this.blockSize * this.scale
+            );
+         }
+      }
+   }
 
-	reset() {
-		this.destroyed = false;
-		this.obstacle = this.fullObstacle;
-	}
+   reset() {
+      this.destroyed = false;
+      this.obstacle = this.fullObstacle;
+      this.totalBlocks = this.countBlocks();
+   }
 
-	// hit() {
-	// 	this.state++;
-	// 	if (this.state >= this.obstacleStates.state.length) {
-	// 		this.destroyed = true;
-	// 		return;
-	// 	}
+   // hit() {
+   // 	this.state++;
+   // 	if (this.state >= this.obstacleStates.state.length) {
+   // 		this.destroyed = true;
+   // 		return;
+   // 	}
 
-	// 	this.obstacle = this.obstacleStates.state[this.state].map;
-	// }
+   // 	this.obstacle = this.obstacleStates.state[this.state].map;
+   // }
 
-	checkBulletCollision() {
-		this.obstacle.forEach((row) => {
-			if (!obstacle.destroyed) {
-				if (this.playerBC.collideWith(obstacle) || this.enemyBC.collideWith(obstacle)) {
-					obstacle.hit();
-				}
+   checkBulletCollision() {
+      // TODO: Change this so that it checks individual blocks
+      if (!this.ready || this.destroyed) return;
+      let object = {};
+      for (let y = 0; y < this.obstacle.map.length; y++) {
+         for (let x = 0; x < this.obstacle.map[y].length; x++) {
+            object = {
+               x: x * this.blockSize * this.scale + this.x,
+               y: y * this.blockSize * this.scale + this.y,
+               width: this.blockSize * this.scale,
+               height: this.blockSize * this.scale,
+               scale: this.scale,
+            };
+         }
+      }
+   }
 
-			}
-			if (!this.c) {
-				obstacle.map.forEach((row) => {
-					let rowChar = '';
-					row.forEach((col) => {
-						rowChar += col;
-					});
-					console.log(rowChar);
-				});
-				this.c = true;
-			}
-		});
-	}
-
+   countBlocks() {
+      let blocks = 0;
+      for (let y = 0; y < this.obstacle.map.length; y++) {
+         for (let x = 0; x < this.obstacle.map[y].length; x++) {
+            if (this.obstacle.map[y][x] === '#') {
+               blocks++;
+            }
+         }
+      }
+      return blocks;
+   }
 }
