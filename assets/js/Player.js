d024f4c22b8501d9422c1473091c245d8b7eba0a
Christian Wiehl Hejlesen <wiehlhejlesen@outlook.dk>
2022-10-28 23:58:33 +0200

    Oops

--- assets/js/Player.js
+++ assets/js/Player.js
@@ -1,121 +1,117 @@
 /** @format */
 
 export default class Player {
-	//---VARIABLES--//
-	shoot = false;
-	doneLoadingAssets = false;
-	numberOfAssets = 0;
-	assets = null;
-	vx = 0;
-	defaultLives = 1;
-	lives = this.defaultLives;
-	yOffset = 37;
+   //---VARIABLES--//
+   shoot = false;
+   doneLoadingAssets = false;
+   numberOfAssets = 0;
+   assets = null;
+   vx = 0;
+   defaultLives = 3;
+   lives = this.defaultLives;
+   yOffset = 37;
 
-	assetsToLoad = [
-		{ id: 1, var: 'image', src: '../assets/gfx/Player.png' },
-	];
+   assetsToLoad = [{ id: 1, var: 'image', src: '../assets/gfx/Player.png' }];
 
-	constructor(canvas, bulletController, scale = 2, speed = 2) {
-		this.scale = scale;
-		this.speed = speed;
-		this.canvas = canvas;
-		this.bulletController = bulletController;
-		this.loadAssets()
-	}
-	constructor2() {
-		this.width = this.image.width;
-		this.height = this.image.height;
-		this.defaultX = this.canvas.width / 2 - (this.width * this.scale) / 2;
-		this.x = this.defaultX;
-		this.y = this.canvas.height - this.height * this.scale - this.yOffset;
-	}
+   constructor(canvas, bulletController, scale = 2, speed = 2) {
+      this.scale = scale;
+      this.speed = speed;
+      this.canvas = canvas;
+      this.bulletController = bulletController;
+      this.loadAssets();
+   }
+   constructor2() {
+      this.width = this.image.width;
+      this.height = this.image.height;
+      this.defaultX = this.canvas.width / 2 - (this.width * this.scale) / 2;
+      this.x = this.defaultX;
+      this.y = this.canvas.height - this.height * this.scale - this.yOffset;
+   }
 
-	newGame() {
-		this.vx = 0;
-		this.x = this.defaultX;
-		this.lives = this.defaultLives;
-		this.bulletController.bullets = [];
-		this.shoot = false;
-	}
+   newGame() {
+      this.vx = 0;
+      this.x = this.defaultX;
+      this.lives = this.defaultLives;
+      this.bulletController.bullets = [];
+      this.shoot = false;
+   }
 
-	dies() {
-		this.vx = 0;
-		this.x = this.defaultX;
-		this.lives--;
-	}
+   dies() {
+      this.vx = 0;
+      this.x = this.defaultX;
+      this.lives--;
+   }
 
-	update(keyboard) {
-		this.input(keyboard);
-		this.checkBoundaries();
-		this.shootBullet();
-		this.x += this.vx * this.speed;
-	}
+   update(keyboard) {
+      this.input(keyboard);
+      this.checkBoundaries();
+      this.shootBullet();
+      this.x += this.vx * this.speed;
+   }
 
-	checkBoundaries() {
-		if (this.x < 0) {
-			this.x = 0;
-		} else if (this.x + this.width * this.scale > this.canvas.width) {
-			this.x = this.canvas.width - this.width * this.scale;
-		}
-	}
+   checkBoundaries() {
+      if (this.x < 0) {
+         this.x = 0;
+      } else if (this.x + this.width * this.scale > this.canvas.width) {
+         this.x = this.canvas.width - this.width * this.scale;
+      }
+   }
 
-	draw(ctx) {
-		ctx.drawImage(this.image, this.x, this.y, this.width * this.scale, this.height * this.scale);
-		for (let i = 0; i < this.lives - 1; i++) {
-			ctx.drawImage(this.image, i * 30 + 60, 540, this.width, this.height);
-		}
-	}
+   draw(ctx) {
+      ctx.drawImage(this.image, this.x, this.y, this.width * this.scale, this.height * this.scale);
+      for (let i = 0; i < this.lives - 1; i++) {
+         ctx.drawImage(this.image, i * 30 + 60, 540, this.width, this.height);
+      }
+   }
 
-	shootBullet() {
-		if (this.shoot) {
-			this.bulletController.shoot(this.x + (this.width * this.scale) / 2, this.y, 4, 10);
-		}
-	}
+   shootBullet() {
+      if (this.shoot) {
+         this.bulletController.shoot(this.x + (this.width * this.scale) / 2, this.y, 4, 10);
+      }
+   }
 
-	input(keyboard) {
-		switch (keyboard.key) {
-			case 'ArrowLeft':
-				this.vx = keyboard.type == 'keydown' ? -1 : 0;
-				break;
-			case 'ArrowRight':
-				this.vx = keyboard.type == 'keydown' ? 1 : 0;
-				break;
-			case ' ':
-				this.shoot = keyboard.type === 'keydown' ? true : false;
-				break;
-		}
-	}
+   input(keyboard) {
+      switch (keyboard.key) {
+         case 'ArrowLeft':
+            this.vx = keyboard.type == 'keydown' ? -1 : 0;
+            break;
+         case 'ArrowRight':
+            this.vx = keyboard.type == 'keydown' ? 1 : 0;
+            break;
+         case ' ':
+            this.shoot = keyboard.type === 'keydown' ? true : false;
+            break;
+      }
+   }
 
-	//-----ASSETSLOADER-----//
-	loadAssets() {
-		if (!this.assetsToLoad || this.assetsToLoad.length == 0) {
-			this.constructor2();
-			return;
-		}
-		if (this.assetsToLoad) {
-			this.numberOfAssets = this.assetsToLoad.length;
+   //-----ASSETSLOADER-----//
+   loadAssets() {
+      if (!this.assetsToLoad || this.assetsToLoad.length == 0) {
+         this.constructor2();
+         return;
+      }
+      if (this.assetsToLoad) {
+         this.numberOfAssets = this.assetsToLoad.length;
 
-			for (let i = 0; i < this.assetsToLoad.length; i++) {
-				if (this.assetsToLoad[i].var != undefined && this.assetsToLoad[i].src != undefined) {
-					this.beginLoadingImage(
-						this.assetsToLoad[i].var,
-						this.assetsToLoad[i].src);
-				}
-			}
-		}
-	}
+         for (let i = 0; i < this.assetsToLoad.length; i++) {
+            if (this.assetsToLoad[i].var != undefined && this.assetsToLoad[i].src != undefined) {
+               this.beginLoadingImage(this.assetsToLoad[i].var, this.assetsToLoad[i].src);
+            }
+         }
+      }
+   }
 
-	launchIfReady() {
-		this.numberOfAssets--;
-		if (this.numberOfAssets == 0) {
-			this.constructor2();
-		}
-	}
+   launchIfReady() {
+      this.numberOfAssets--;
+      if (this.numberOfAssets == 0) {
+         this.constructor2();
+      }
+   }
 
-	beginLoadingImage(imgVar, fileName) {
-		eval(`this.${imgVar} = new Image();`);
-		eval(`this.${imgVar}`).onload = () => this.launchIfReady();
-		eval(`this.${imgVar}.src = '${fileName}';`);
-	}
-	//-----END OF ASSETSLOADER-----//
+   beginLoadingImage(imgVar, fileName) {
+      eval(`this.${imgVar} = new Image();`);
+      eval(`this.${imgVar}`).onload = () => this.launchIfReady();
+      eval(`this.${imgVar}.src = '${fileName}';`);
+   }
+   //-----END OF ASSETSLOADER-----//
 }
